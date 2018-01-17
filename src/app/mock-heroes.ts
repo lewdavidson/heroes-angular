70ec4cdf370c9aaa8d845f3dc29afbf2a6ad9243
Guest User <Guest@Epicodus-3C.local>
2018-01-16 16:16:06 -0800

    add most things up to step 4

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Lice' },
+  { id: 12, name: 'Marco' },
+  { id: 13, name: 'Bombassi' },
+  { id: 14, name: 'Celery' },
+  { id: 15, name: 'Magnet' },
+  { id: 16, name: 'CondomMan' },
+  { id: 17, name: 'Dyno-myte' },
+  { id: 18, name: 'Dr Lick' },
+  { id: 19, name: 'SMagma' },
+  { id: 20, name: 'Tomaado' }
+];
